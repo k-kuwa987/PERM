add4bc25b65db1ba5baed1ced606198fcdd3cd37
k-kuwa987 <k-kuwa987@akane.waseda.jp>
2021-02-23 22:08:38 +0900

    delete card

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,14 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to PERspective Manager
-        </v-card-title>
-        <v-card-text>
-          <p>
-            これはバスに関する情報を様々なユーザーの視点から確認する際のデモです．
-          </p>
+  <div>
+    <h2>Welcome to PERspective Manager</h2>
+    <p>
+      これはバスに関する情報を様々なユーザーの視点から確認する際のデモです．
+    </p>
 
-          <p>
-            各視点ごとに見ることができるコンテンツに制限や加工が適切に施されています．
-          </p>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+    <p>
+      各視点ごとに見ることができるコンテンツに制限や加工が適切に施されています．
+    </p>
+  </div>
 </template>
 
 <script>
