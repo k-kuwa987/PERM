ee1aeadefcd6041f5afd973e3a748079c23a4545
k-kuwa987 <k-kuwa987@akane.waseda.jp>
2021-03-04 10:07:28 +0900

    add images from gan

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h2>Admin</h2>
-    <p>{{ resp }}</p>
+    <img v-bind:src="'data:image/jpeg;base64,' + resp.slice(0, -4)" />
   </div>
 </template>
 <script>
 export default {
   async asyncData({ $axios }) {
     // 取得先のURL
-    const url = "http://localhost:1880/hogehoge";
+    const url = "http://localhost:1880/admin";
     // リクエスト（Get）
     const response = await $axios.$get(url);
     // 配列で返ってくるのでJSONにして返却
